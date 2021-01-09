a81c652456b6a96e1aa8014769efe02eab207f5a
ede <m18060960803@163.com>
2021-01-09 12:43:27 +0800

    Java

---
+++ java-starter/src/test/java/com/ldx/JavaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
